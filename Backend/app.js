abf325a97d63d912f78f0f454f5f0dcb98b73e71
HarshalMakode <harshalmakode26@gmial.com>
2024-10-06 15:08:54 +0530

    Initial vercel

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 const studentRoute = require('./routes/student.route.js');
 const app = express();
 
@@ -24,9 +25,10 @@ app.get("/api/students", (req, res) => {
 
 mongoose.connect("mongodb+srv://harshalmakode26:K6woOov6pvvf831o@cruddb.mlexk.mongodb.net/?retryWrites=true&w=majority&appName=crudDB")
 .then(() => {
+    const PORT = process.env.PORT || 3000; 
     console.log("Connected to database!");
-    app.listen(3000, ()=>{
-        console.log('Server is running on port 3000');
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`);
     });
 })
 .catch(() => {
