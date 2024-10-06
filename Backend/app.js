179a4a3fe35c9f816b5d82a42f510df669de6b83
HarshalMakode <harshalmakode26@gmial.com>
2024-10-06 13:24:14 +0530

    local only

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const studentRoute = require('../routes/student.route.js');
+const studentRoute = require('./routes/student.route.js');
 const app = express();
 
 //middleware
